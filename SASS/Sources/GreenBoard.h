04742f1bc334a6e806da301fadf33359c1db9b05
Joseph Walters <joseph.b .walters.97@gmail.com>
2019-01-10 21:53:46 -0500

    Added board headers

---
+++ SASS/Sources/GreenBoard.h
@@ -0,0 +1,57 @@
+#ifndef GREENBOARD_H_
+#define GREENBOARD_H_
+
+#include <gpio>
+
+#include "Board.h"
+
+
+#define RADAR_N_PORT
+#define RADAR_E_PORT
+#define RADAR_S_PORT
+#define RADAR_W_PORT
+
+#define RADAR_N_PIN
+#define RADAR_E_PIN
+#define RADAR_S_PIN
+#define RADAR_W_PIN
+
+
+#define LIDAR_N_PORT
+#define LIDAR_E_PORT
+#define LIDAR_S_PORT
+#define LIDAR_W_PORT
+
+#define LIDAR_N_PIN
+#define LIDAR_E_PIN
+#define LIDAR_S_PIN
+#define LIDAR_W_PIN
+
+
+#define RED_N_PORT GPIO_PORT_P7
+#define RED_E_PORT GPIO_PORT_P7
+#define RED_S_PORT
+#define RED_W_PORT
+
+#define RED_N_PIN GPIO_PIN4
+#define RED_E_PIN GPIO_PIN5
+#define RED_S_PIN
+#define RED_W_PIN
+
+
+#define YELLOW_N_PORT GPIO_PORT_P7
+#define YELLOW_E_PORT GPIO_PORT_P7
+#define YELLOW_S_PORT
+#define YELLOW_W_PORT
+
+#define YELLOW_N_PIN GPIO_PIN6
+#define YELLOW_E_PIN GPIO_PIN7
+#define YELLOW_S_PIN
+#define YELLOW_W_PIN
+
+
+
+
+
+
+#endif // GREENBOARD_H_
---
+++ SASS/Sources/RedBoard.h
@@ -0,0 +1,8 @@
+#ifndef SOURCES_REDBOARD_H_
+#define SOURCES_REDBOARD_H_
+
+#include "Board.h"
+
+
+
+#endif // REDBOARD_H_

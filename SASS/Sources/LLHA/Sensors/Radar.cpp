3a6bd7168eddf50da18e71ef693d8ac159e63b4a
Trent Sellers <sellerstj@bellsouth.net>
2019-02-19 12:42:03 -0500

    Added to Radar class to implement RADAR access.

--- SASS/Sources/LLHA/Sensors/Radar.cpp
+++ SASS/Sources/LLHA/Sensors/Radar.cpp
@@ -1,9 +1,39 @@
+#define __MSP432P401R__
+
+#include <xdc/runtime/System.h>
+#include <xdc/std.h>
+
+#include <Board.h>
+#include <Sources/GreenBoard.h>
+
+#include <Sources/Utils/Logger.h>
 #include <Sources/LLHA/Sensors/Radar.h>
 
 using namespace sources::llha::sensors;
+using namespace sources::utils;
 
-Radar::Radar()
+Radar::Radar(RadarInstanceType radar_type) : m_radar_type(radar_type)
 {
+    Logger::print((String)"Creating instance...");
+
+    switch(radar_type)
+    {
+    case RADAR_NORTH:
+        m_hardware_module = RADAR_N;
+        break;
+    case RADAR_EAST:
+        m_hardware_module = RADAR_E;
+        break;
+    case RADAR_SOUTH:
+        m_hardware_module = RADAR_S;
+        break;
+    case RADAR_WEST:
+        m_hardware_module = RADAR_W;
+        break;
+    default:
+        // TODO: Throw exception
+    };
+
     init();
 }
 
@@ -23,7 +53,7 @@ Radar* Radar::get_instance(RadarInstanceType radar_type)
         }
         else
         {
-            radar_north = new Radar();
+            radar_north = new Radar(radar_type);
             return radar_north;
         }
     case RADAR_EAST:
@@ -33,7 +63,7 @@ Radar* Radar::get_instance(RadarInstanceType radar_type)
         }
         else
         {
-            radar_east = new Radar();
+            radar_east = new Radar(radar_type);
             return radar_east;
         }
     case RADAR_SOUTH:
@@ -43,7 +73,7 @@ Radar* Radar::get_instance(RadarInstanceType radar_type)
         }
         else
         {
-            radar_south = new Radar();
+            radar_south = new Radar(radar_type);
             return radar_south;
         }
     case RADAR_WEST:
@@ -53,17 +83,43 @@ Radar* Radar::get_instance(RadarInstanceType radar_type)
         }
         else
         {
-            radar_west = new Radar();
+            radar_west = new Radar(radar_type);
             return radar_west;
         }
     default:
-        return nullptr;
+        return nullptr; // TODO: Throw exception
     };
+}
+
+void Radar::init()
+{
+    SPI_init();  // Initialize the SPI driver
+
+    SPI_Params_init(&spiParams);  // Initialize SPI parameters
+    spiParams.dataSize = 8;       // 8-bit data size
 }
 
 double Radar::get_distance()
 {
-    // TEMPORARY RETURN
+    spi = SPI_open(Board_SPI0, &spiParams);
+    if (spi == NULL)
+    {
+        while (1);  // SPI_open() failed
+    }
+
+    // Fill in transmitBuffer
+
+    spiTransaction.count = 10; // Need to reconsider message size
+    spiTransaction.txBuf = (void *)transmitBuffer;
+    spiTransaction.rxBuf = (void *)receiveBuffer;
+
+    transferOK = SPI_transfer(spi, &spiTransaction);
+    if (!transferOK)
+    {
+        // Error in SPI or transfer already in progress.
+        while (1);
+    }
+
     return 0.00;
 }
 
@@ -71,9 +127,4 @@ double Radar::get_velocity()
 {
     // TEMPORARY RETURN
     return 0.00;
-}
-
-void Radar::init()
-{
-
 }
--- SASS/Sources/LLHA/Sensors/Radar.h
+++ SASS/Sources/LLHA/Sensors/Radar.h
@@ -1,6 +1,8 @@
 #ifndef RADAR_H_
 #define RADAR_H_
 
+#include <ti/drivers/SPI.h>
+
 enum RadarInstanceType
 {
     RADAR_NORTH,
@@ -27,8 +29,19 @@ public:
     void init();
 
 private:
-    Radar();
+    Radar(RadarInstanceType radar_type);
     virtual ~Radar();
+
+    RadarInstanceType m_radar_type;
+    uint_least8_t m_hardware_module;
+
+    // SPI-related variables
+    SPI_Handle      spi;
+    SPI_Params      spiParams;
+    SPI_Transaction spiTransaction;
+    uint8_t         transmitBuffer[10]; // Need to reconsider message size
+    uint8_t         receiveBuffer[10];
+    bool            transferOK;
 
     static Radar* radar_north;
     static Radar* radar_east;
